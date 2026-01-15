ec9283d4c4877c7f178a6407446c56915522051a
Garipov Nadir <nadir_garipov@mail.ru>
2026-01-15 12:57:24 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-portfolio/',
+  base: 'vite-portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
